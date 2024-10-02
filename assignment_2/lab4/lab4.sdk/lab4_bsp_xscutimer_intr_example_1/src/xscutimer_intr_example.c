7ab37210ffe18940ed63d36c18a33db9917a07c7
andreasp <andreas.s.p00@gmail.com>
2024-10-02 12:43:04 +0200

    working interrupt based timer

--- assignment_2/lab4/lab4.sdk/lab4_bsp_xscutimer_intr_example_1/src/xscutimer_intr_example.c
+++ assignment_2/lab4/lab4.sdk/lab4_bsp_xscutimer_intr_example_1/src/xscutimer_intr_example.c
@@ -1,18 +1,64 @@
+/******************************************************************************
+*
+* Copyright (C) 2010 - 2014 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xscutimer_intr_example.c
+*
+* This file contains a design example using the Cortex A9 Scu Private
+* Timer and the driver (XScuTimer) using interrupts.
+*
+* @note		None.
+*
+* <pre>
+*
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date     Changes
+* ----- ---- -------- ---------------------------------------------
+* 1.00a nm   03/10/10 First release
+* </pre>
+*
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+
 #include "xparameters.h"
-#include "xgpio.h"
 #include "xscutimer.h"
 #include "xscugic.h"
 #include "xil_exception.h"
 #include "xil_printf.h"
-#include "led_ip.h"
-#include "sleep.h"
 
-//====================================================
-
-void read_switches(XGpio dip);
-int configure_timer(XScuTimer timer);
-unsigned long delay = 100000; // useconds
-#define ONE_SECOND 325000000
+/************************** Constant Definitions *****************************/
 
 /*
  * The following constants map to the XPAR parameters created in the
@@ -27,6 +73,12 @@ unsigned long delay = 100000; // useconds
 
 #define TIMER_LOAD_VALUE	0xFFFF
 
+/**************************** Type Definitions *******************************/
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+/************************** Function Prototypes ******************************/
+
 int ScuTimerIntrExample(XScuGic *IntcInstancePtr, XScuTimer *TimerInstancePtr,
 			u16 TimerDeviceId, u16 TimerIntrId);
 
@@ -37,8 +89,12 @@ static int TimerSetupIntrSystem(XScuGic *IntcInstancePtr,
 
 static void TimerDisableIntrSystem(XScuGic *IntcInstancePtr, u16 TimerIntrId);
 
+/************************** Variable Definitions *****************************/
+
+#ifndef TESTAPP_GEN
 XScuTimer TimerInstance;	/* Cortex A9 Scu Private Timer Instance */
 XScuGic IntcInstance;		/* Interrupt Controller Instance */
+#endif
 
 /*
  * The following variables are shared between non-interrupt processing and
@@ -46,12 +102,23 @@ XScuGic IntcInstance;		/* Interrupt Controller Instance */
  */
 volatile int TimerExpired;
 
-int main (void) 
+/*****************************************************************************/
+/**
+* Main function to call the Cortex A9 Scu Private Timer interrupt example.
+*
+* @param	None.
+*
+* @return	XST_SUCCESS if successful, otherwise XST_FAILURE.
+*
+* @note		None.
+*
+******************************************************************************/
+#ifndef TESTAPP_GEN
+int main(void)
 {
 	int Status;
 
-	Status = ScuTimerIntrExample(&IntcInstance, &TimerInstance,
-				TIMER_DEVICE_ID, TIMER_IRPT_INTR);
+	xil_printf("SCU Timer Interrupt Example Test \r\n");
 
 	/*
 	 * Call the interrupt example, specify the parameters generated in
@@ -66,42 +133,26 @@ int main (void)
 
 	xil_printf("Successfully ran SCU Timer Interrupt Example Test\r\n");
 	return XST_SUCCESS;
-
-	XGpio dip, push;
- 
-	XGpio_Initialize(&dip, XPAR_SWITCHES_DEVICE_ID);
-	XGpio_SetDataDirection(&dip, 1, 0xffffffff);
-	
-	XGpio_Initialize(&push, XPAR_BUTTONS_DEVICE_ID);
-	XGpio_SetDataDirection(&push, 1, 0xffffffff);
-
-	int value, skip;
-
-	xil_printf("\r\n-- Start of the Program --\r\n");
-
-	while (1)
-	{
-	  xil_printf("\r\nCMD:> ");
-	  value = inbyte();
-	  xil_printf("%c", value);
-	  skip = inbyte();
-	  switch (value)
-	  {
-	  	  case '1':
-	  		  xil_printf("SW->Leds");
-	  		  read_switches(dip);
-	  		  break;
-	  	  case '2':
-	  		  xil_printf("\r\nReceived 2.");
-	  		  break;
-	  	  default:
-	  		  xil_printf("\r\nUnrecognized input.");
-	  		  break;
-	  }
-	  usleep(delay);
-	}
 }
+#endif
 
+/*****************************************************************************/
+/**
+*
+* This function tests the functioning of the Cortex A9 Scu Private Timer driver
+* and hardware using interrupts.
+*
+* @param	IntcInstancePtr is a pointer to the instance of XScuGic driver.
+* @param	TimerInstancePtr is a pointer to the instance of XScuTimer
+*		driver.
+* @param	TimerDeviceId is the Device ID of the XScuTimer device.
+* @param	TimerIntrId is the Interrupt Id of the XScuTimer device.
+*
+* @return	XST_SUCCESS if successful, otherwise XST_FAILURE.
+*
+* @note		None.
+*
+******************************************************************************/
 int ScuTimerIntrExample(XScuGic *IntcInstancePtr, XScuTimer * TimerInstancePtr,
 			u16 TimerDeviceId, u16 TimerIntrId)
 {
@@ -150,7 +201,7 @@ int ScuTimerIntrExample(XScuGic *IntcInstancePtr, XScuTimer * TimerInstancePtr,
 	/*
 	 * Load the timer counter register.
 	 */
-	XScuTimer_LoadTimer(TimerInstancePtr, ONE_SECOND);
+	XScuTimer_LoadTimer(TimerInstancePtr, TIMER_LOAD_VALUE);
 
 	/*
 	 * Start the timer counter and then wait for it
@@ -297,7 +348,6 @@ static void TimerIntrHandler(void *CallBackRef)
 	if (XScuTimer_IsExpired(TimerInstancePtr)) {
 		XScuTimer_ClearInterruptStatus(TimerInstancePtr);
 		TimerExpired++;
-		xil_printf("\r\nINTERRUPT!");
 		if (TimerExpired == 3) {
 			XScuTimer_DisableAutoReload(TimerInstancePtr);
 		}
@@ -324,15 +374,4 @@ static void TimerDisableIntrSystem(XScuGic *IntcInstancePtr, u16 TimerIntrId)
 	 * Disconnect and disable the interrupt for the Timer.
 	 */
 	XScuGic_Disconnect(IntcInstancePtr, TimerIntrId);
-}
-
-void read_switches (XGpio dip)
-{
-	int switch_read = XGpio_DiscreteRead(&dip, 1);
-	LED_IP_mWriteReg(XPAR_LED_IP_S_AXI_BASEADDR, 0, switch_read);
-}
-
-void count_leds (XGpio dip)
-{
-	xil_printf("Test");
 }
