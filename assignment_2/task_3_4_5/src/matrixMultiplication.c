23441a9dfc52f75d6c471b072fbbc645ad7e82b1
andreasp <andreas.s.p00@gmail.com>
2024-10-06 12:04:05 +0200

    DROPME! matrixMultiplication.c formatting
    Looks better in LaTeX minted with no empty spaces between lines.

--- assignment_2/task_3_4_5/src/matrixMultiplication.c
+++ assignment_2/task_3_4_5/src/matrixMultiplication.c
@@ -1,46 +1,33 @@
 #include "matrixMultiplication.h"
 #include "xil_io.h"
 
-void setInputMatrices(vectorArray matrixA, vectorArray matrixB){
-
-    for (int row = 0; row < MATRIX_SIZE; row++){
-
-        for (int col = 0; col < MATRIX_SIZE; col++){
-
+void setInputMatrices(vectorArray matrixA, vectorArray matrixB)
+{
+    for (int row = 0; row < MATRIX_SIZE; row++) {
+        for (int col = 0; col < MATRIX_SIZE; col++) {
           matrixA[row].comp[col] = MATRIX_SIZE * row + 1 + col;
           matrixB[row].comp[col] = row + 1;
-
         }
     }
 }
 
-void displayMatrix(vectorArray matrix){
-
-    for (int row = 0; row < MATRIX_SIZE; row++){
-
-        for (int col = 0; col < MATRIX_SIZE; col++){
-
+void displayMatrix(vectorArray matrix)
+{
+    for (int row = 0; row < MATRIX_SIZE; row++)  {
+        for (int col = 0; col < MATRIX_SIZE; col++) {
             xil_printf("%d\t", matrix[row].comp[col]);
-
         }
-
         xil_printf("\r\n");
-
     }
 }
 
-void multiplyMatricesSoft(vectorArray matrixA, vectorArray matrixB, vectorArray matrixP){
-
-    for (int row = 0; row < MATRIX_SIZE; row++){
-
-        for (int col = 0; col < MATRIX_SIZE; col++){
-
+void multiplyMatricesSoft(vectorArray matrixA, vectorArray matrixB, vectorArray matrixP)
+{
+    for (int row = 0; row < MATRIX_SIZE; row++) {
+        for (int col = 0; col < MATRIX_SIZE; col++) {
             matrixP[row].comp[col] = 0;
-
-            for (int i = 0; i < MATRIX_SIZE; i++){
-
+            for (int i = 0; i < MATRIX_SIZE; i++) {
                 matrixP[row].comp[col] += matrixA[row].comp[i] * matrixB[col].comp[i];
-
             }
         }
     }
