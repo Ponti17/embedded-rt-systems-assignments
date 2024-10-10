f161828c713a9bb9d0a350984cc6ea5984b53281
andreasp <90260734+Ponti17@users.noreply.github.com>
2024-10-10 08:27:32 +0200

    Honey a bomb exploded in our repo (#36)
    Things that are HASTA LA VISTA:
    - Kims SystemC examples (were they really useful??????)
    - Our SystemC template (never used. Better to just copy our assignment
    project)

--- examples/TemplateFir/SystemCraft.bat
+++
@@ -1,1 +0,0 @@
-"C:\Programmer\SystemCrafter\SystemCrafter SC\bin\craft.exe" /vhdl /oh SynFirGate.h /oc SynFirGate.cpp SynFir.cpp
--- examples/simple_process_ex/simple_process_ex.cpp
+++
@@ -1,7 +0,0 @@
-
-#include "simple_process_ex.h"
-void simple_process_ex::my_thread_process(void) {
-  std::cout << "my_thread_process executed within "
-            << name() 
-            << std::endl;
-}
