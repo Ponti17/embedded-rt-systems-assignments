e7f2b09ea121051c8e9b1a37df62529785d5c67b
Alexandre Thorkil Cherencq <201506019@post.au.dk>
2024-10-22 12:31:27 +0200

    class defs

--- project/game_engine/GameScene.hpp
+++ project/game_engine/GameScene.hpp
@@ -5,9 +5,22 @@
 #ifndef GAMESCENE_H
 #define GAMESCENE_H
 
+#include "Types.hpp"
+#include "GameObject.hpp"
+
+#include <vector>
+
 namespace Engine {
 
 class GameScene {
+
+    public:
+        std::vector<GameObject *> gameObjects;
+
+        void update(float deltaTime) {
+          for (auto & object : this->gameObjects) {
+            object->update(deltaTime);
+            };
 
 };
 
--- project/game_engine/Types.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by alext on 22/10/2024.
-//
-
-#include "Types.h"
-
-namespace Engine {
-} // Engine
