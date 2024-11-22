2f9fc1a8d0252c2821a674c2d34982d71d37cafd
Alexandre Thorkil Cherencq <201506019@post.au.dk>
2024-11-22 10:53:13 +0100

    add singleton instances

--- assignment_3/src/states.cpp
+++ assignment_3/src/states.cpp
@@ -2,8 +2,21 @@
 
 using namespace states;
 
+// Singleton instances
 PowerOnSelfTest* PowerOnSelfTest::instance = nullptr;
+Failure* Failure::instance = nullptr;
+Initializing* Initializing::instance = nullptr;
+Ready* Ready::instance = nullptr;
+Configuration* Configuration::instance = nullptr;
+RealTimeLoop* RealTimeLoop::instance = nullptr;
+Suspended* Suspended::instance = nullptr;
 
 void PowerOnSelfTest::selfTestOk(EmbeddedSystemX* context) {
     context->changeState(Initializing::getInstance());
 } 
+
+void PowerOnSelfTest::selfTestFailed(EmbeddedSystemX* context, int errno) {
+    context->displayError(errno);
+    context->changeState(Failure::getInstance());
+}
+
