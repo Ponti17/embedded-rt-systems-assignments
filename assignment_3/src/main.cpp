191499bf3fe1f6ea214c8536f2f95b7a12ac195a
Alexandre Thorkil Cherencq <201506019@post.au.dk>
2024-11-22 11:50:58 +0100

    main file for testing

---
+++ assignment_3/src/main.cpp
@@ -0,0 +1,13 @@
+#include "states.h"
+#include "embeddedSystemX.h"
+
+#include <iostream>
+
+
+int main(int argc, char const *argv[])
+{
+    
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
