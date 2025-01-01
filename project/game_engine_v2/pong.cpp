309074d25536ebc4bba951d0bbca67f3e54f8bef
Alexandre Thorkil Cherencq <201506019@post.au.dk>
2025-01-01 20:07:51 +0100

    add more logic

--- project/game_engine_v2/pong.cpp
+++ project/game_engine_v2/pong.cpp
@@ -7,15 +7,23 @@ ExitGame * ExitGame::instance = nullptr;
 
 int main(int argc, char** argv) {
 
-    Paddle * player1, * player2;
-    Ball* ball;
+    Paddle *player1 = new Paddle();
+    Paddle *player2 = new Paddle();
+    Ball *ball = new Ball();
+
+    player1->shape->position = {0, 0};
+    player2->shape->position = {0, 0};
+
+    ball->shape->position = {0, 0};
+
+    ball->velocity = {0.0f, 0.0f};
+
 
     PlayingGame::getInstance()->gameObjects.push_back(player1);
     PlayingGame::getInstance()->gameObjects.push_back(player2);
     PlayingGame::getInstance()->gameObjects.push_back(ball);
     
-    auto PongTheGame = new GameContext();
-
+    auto PongTheGame = std::make_unique<GameContext>();
     PongTheGame->gameLoop();
 
     return 0;
